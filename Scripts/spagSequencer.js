c4eb2b118576181f757176e2967296142bb43473
Gabriel Obregon <gobregon@summa-tech.com>
2013-03-22 14:48:02 -0400

    Options are working. Bunch of other stuff done. Trying to get custom events to work

--- Scripts/spagSequencer.js
+++ Scripts/spagSequencer.js
@@ -1,32 +1,80 @@
 (function($){
 
+	var SequenceEvent = function( step, options )
+	{
+		this.step = step;
+		this.options = $.extend({}, this.defaults, options);
+	};
+
+	SequenceEvent.prototype =
+	{
+		constructor: SequenceEvent,
+		
+		defaults:
+		{
+			fire: function(){},
+			done: function(){}
+		},
+
+		init: function()
+		{
+			$(this).on("fire" + this.step, this.options.fire());
+			//$.off("afterFire" + this.step, this.options.done());
+		}
+	};
+
 	var SpagSequencer = function( element, options )
 	{
 		this.element = $(element);
 		this.options = $.extend({}, $.fn.spagSequencer.defaults, options);
+
+		this.commandSequence = [];
+
+		this.prepareSequence();
 	};
 
-	SpagSequencer.prototype = 
+	SpagSequencer.prototype =
 	{
 		// TODO: Understand constructor in the prototype and lowercase.
 		constructor: SpagSequencer,
 
-		setOptions: function(options)
-		{
-
-			$(this).each( function(key, value){
-				this.setOption(key, value);
-			});
-		},
-
-		setOption: function(key, value)
+		setOption: function (key, value)
 		{
 			this.options[key] = value;
+			this.prepareSequence();
+		},
+
+		prepareSequence: function()
+		{
+			this.commandSequence.length = this.options.steps;
+			// for (var i = 0; i < this.options.steps; i++) {
+			// 	$.
+			// };
+		},
+
+		addEvent: function( step, calls )
+		{
+			// TODO: This may not be needed to be put in an array.
+			this.commandSequence.push( new SequenceEvent( step, calls ) );
+		},
+
+		getDelayTime: function()
+		{
+			var beatPerSecond = this.options.bpm / 60;
+			return beatPerSecond / (this.options.steps / 4);
+		},
+
+		play: function()
+		{
+			// TODO: Finish this
+
 		}
 	};
 
 	$.fn.spagSequencer = function( options )
 	{
+		var args = Array.prototype.slice.call(arguments, 1);
+
 		return this.each( function()
 			{
 				var $this = $(this);
@@ -34,9 +82,9 @@
 				if(!data) {
 					$this.data( 'spagSequencer', (data = new SpagSequencer(this, options)) );
 				}
-				if( typeof options == 'string'){
-					var args = Array.prototype.slice.call( arguments, 1 );
-					data[options].apply( data, args );
+				if(typeof options == "string")
+				{
+					data[options].apply(data, args);
 				}
 			});
 	};

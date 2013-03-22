ba9147f749a9c4b9e4bc9731b694e200d2a92de9
Gabriel Obregon <gobregon@summa-tech.com>
2013-03-22 14:45:13 -0400

    Options are working. Bunch of other stuff done. Trying to get custom events to work

--- test/spagSequencer.Test.js
+++ test/spagSequencer.Test.js
@@ -1,24 +1,53 @@
 describe( "Sequencer", function()
 {
-	var sequencer;
+	var sequencer, instance;
 	beforeEach( function()
 	{
 		sequencer = $('<div></div>').spagSequencer({
 			steps: 32
 		});
+		instance = sequencer.data('spagSequencer');
+
+		return $(sequencer).appendTo($('body')).html("<div id='PluginPage'>");
+
 	});
 	it("should return a default bpm of 140", function()
 	{
-		expect(sequencer.data('spagSequencer').options.bpm).toEqual(140);
+		expect(instance.options.bpm).toEqual(140);
 	});
 	it("should have 32 steps defined", function()
 	{
-		expect(sequencer.data('spagSequencer').options.steps).toEqual(32);
+		expect(instance.options.steps).toEqual(32);
+	});
+	it("length of command sequence array should be 32", function()
+	{
+		expect(instance.commandSequence.length).toEqual(32);
 	});
 	it("should have a new bpm of 100 and still have steps at 32", function()
 	{
-		sequencer.spagSequencer( "setOptions", "bpm", 100 );
+		sequencer.spagSequencer( "setOption", "bpm", 100 );
 		expect(sequencer.data('spagSequencer').options.bpm).toEqual(100);
 		expect(sequencer.data('spagSequencer').options.steps).toEqual(32);
+	});
+	it("should have a time interval of 1", function()
+	{
+		sequencer.spagSequencer("setOption", "bpm", 60);
+		sequencer.spagSequencer("setOption", "steps", 16);
+		var interval = instance.getDelayTime();
+		expect(interval).toEqual(0.25);
+	});
+	it("should have function added to commandSequence", function()
+	{
+		var sequenceCommand = function(){ console.log("GABO")};
+//		var sequenceCommand = jasmine.createSpy( "sequenceCommand" );
+		sequencer.spagSequencer("addEvent", 1, {fire: sequenceCommand});
+		sequencer.trigger("fire1");
+		expect( sequenceCommand ).toHaveBeenCalled();
+	});
+	it("play should be called", function()
+	{
+		spyOn(instance, 'play');
+		sequencer.spagSequencer('play');
+		expect(instance.play).toHaveBeenCalled();
 	});
 });

bbd72faa72b2dd6dbe53f8a510995c223e6be6ba
Gabriel Obregon <gobregon@summa-tech.com>
2013-03-22 16:49:49 -0400

    Hopefully line endings got fixed.

--- Scripts/spagSequencer.js
+++ Scripts/spagSequencer.js
@@ -1,112 +1,112 @@
 (function($){
 
-	var SequenceEvent = function( element, step, options )
-	{
-		this.element = element;
-		this.step = step;
-		this.options = $.extend({}, this.defaults, options);
-		this.init();
-	};
+    var SequenceEvent = function( element, step, options )
+    {
+        this.element = element;
+        this.step = step;
+        this.options = $.extend({}, this.defaults, options);
+        this.init();
+    };
 
-	SequenceEvent.prototype =
-	{
-		constructor: SequenceEvent,
-		
-		defaults:
-		{
-			fire: function(){},
-			done: function(){}
-		},
+    SequenceEvent.prototype =
+    {
+        constructor: SequenceEvent,
 
-		init: function()
-		{
-			var self = this;
-			this.element.on("fire" + this.step, function(){self.options.fire()});
-			this.element.off("afterFire" + this.step, function(){this.options.done()});
-		}
-	};
+        defaults:
+        {
+            fire: function(){},
+            done: function(){}
+        },
 
-	var SpagSequencer = function( element, options )
-	{
-		this.element = $(element);
-		this.options = $.extend({}, $.fn.spagSequencer.defaults, options);
+        init: function()
+        {
+            var self = this;
+            this.element.on("fire" + this.step, function(){self.options.fire()});
+            this.element.off("afterFire" + this.step, function(){this.options.done()});
+        }
+    };
 
-		this.commandSequence = [];
+    var SpagSequencer = function( element, options )
+    {
+        this.element = $(element);
+        this.options = $.extend({}, $.fn.spagSequencer.defaults, options);
 
-		this.prepareSequence();
-	};
+        this.commandSequence = [];
 
-	SpagSequencer.prototype =
-	{
-		// TODO: Understand constructor in the prototype and lowercase.
-		constructor: SpagSequencer,
+        this.prepareSequence();
+    };
 
-		setOption: function (key, value)
-		{
-			this.options[key] = value;
-			this.prepareSequence();
-		},
+    SpagSequencer.prototype =
+    {
+        // TODO: Understand constructor in the prototype and lowercase.
+        constructor: SpagSequencer,
 
-		prepareSequence: function()
-		{
-			this.commandSequence.length = this.options.steps;
-			var fire;
-			var done;
-			for (var i = 1; i <= this.options.steps; i++) {
-				fire = "fire" + i;
-				done = "done" + i;
-				$.event.trigger({
-					type: fire,
-					message: "Is this necessary? -Gabo",
-					time: new Date()
-				});
-			}
-		},
+        setOption: function (key, value)
+        {
+            this.options[key] = value;
+            this.prepareSequence();
+        },
 
-		addEvent: function( step, calls )
-		{
-			// TODO: This may not be needed to be put in an array.
-			this.commandSequence.push( new SequenceEvent(this.element, step, calls ) );
-		},
+        prepareSequence: function()
+        {
+            this.commandSequence.length = this.options.steps;
+            var fire;
+            var done;
+            for (var i = 1; i <= this.options.steps; i++) {
+                fire = "fire" + i;
+                done = "done" + i;
+                $.event.trigger({
+                    type: fire,
+                    message: "Is this necessary? -Gabo",
+                    time: new Date()
+                });
+            }
+        },
 
-		getDelayTime: function()
-		{
-			var beatPerSecond = this.options.bpm / 60;
-			return beatPerSecond / (this.options.steps / 4);
-		},
+        addEvent: function( step, calls )
+        {
+            // TODO: This may not be needed to be put in an array.
+            this.commandSequence.push( new SequenceEvent(this.element, step, calls ) );
+        },
 
-		play: function()
-		{
-			// TODO: Finish this
+        getDelayTime: function()
+        {
+            var beatPerSecond = this.options.bpm / 60;
+            return beatPerSecond / (this.options.steps / 4);
+        },
 
-		}
-	};
+        play: function()
+        {
+            // TODO: Finish this
 
-	$.fn.spagSequencer = function( options )
-	{
-		var args = Array.prototype.slice.call(arguments, 1);
+        }
+    };
 
-		return this.each( function()
-			{
-				var $this = $(this);
-				var data = $this.data('spagSequencer');
-				if(!data) {
-					$this.data( 'spagSequencer', (data = new SpagSequencer(this, options)) );
-				}
-				if(typeof options == "string")
-				{
-					data[options].apply(data, args);
-				}
-			});
-	};
+    $.fn.spagSequencer = function( options )
+    {
+        var args = Array.prototype.slice.call(arguments, 1);
 
-	// TODO: Understand constructor using the jQuery function and uppercase.
-	$.fn.spagSequencer.Constructor = SpagSequencer;
+        return this.each( function()
+            {
+                var $this = $(this);
+                var data = $this.data('spagSequencer');
+                if(!data) {
+                    $this.data( 'spagSequencer', (data = new SpagSequencer(this, options)) );
+                }
+                if(typeof options == "string")
+                {
+                    data[options].apply(data, args);
+                }
+            });
+    };
 
-	$.fn.spagSequencer.defaults =
-	{
-		bpm:    140,
-		steps:  16
-	};
+    // TODO: Understand constructor using the jQuery function and uppercase.
+    $.fn.spagSequencer.Constructor = SpagSequencer;
+
+    $.fn.spagSequencer.defaults =
+    {
+        bpm:    140,
+        steps:  16
+    };
 
 })(jQuery);

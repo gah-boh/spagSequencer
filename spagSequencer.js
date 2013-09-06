cbf9c1bfdd72af3cd33ddfa24fa23aed52369fd2
Gabo <gabo.obregon@gmail.com>
2013-09-05 21:41:45 -0400

    Code formatting.

--- spagSequencer.js
+++ spagSequencer.js
@@ -1,41 +1,35 @@
 /*Spag Sequencer*/
 (function($){
 
-	var SequenceEvent = function( element, step, options )
-	{
+	var SequenceEvent = function( element, step, options ) {
 		this.element = element;
 		this.step = step;
 		this.options = $.extend({}, this.defaults, options);
 		this.init();
 	};
 
-	SequenceEvent.prototype =
-	{
+	SequenceEvent.prototype = {
 		constructor: SequenceEvent,
 
-		defaults:
-		{
+		defaults: {
 			fire: function(){},
 			afterFire: function(){}
 		},
 
-		init: function()
-		{
+		init: function() {
 			var self = this;
 			this.element.on("fire" + self.step, self.options.fire);
 			this.element.on("afterFire" + self.step, self.options.afterFire);
 		},
 
-		removeEvents: function()
-		{
+		removeEvents: function() {
 			var self = this;
 			this.element.off("fire" + self.step, self.options.fire);
 			this.element.off("afterFire" + self.step, self.options.afterFire);
 		}
 	};
 
-	var SpagSequencer = function( element, options )
-	{
+	var SpagSequencer = function( element, options ) {
 		this.element = $(element);
 		this.options = $.extend({}, $.fn.spagSequencer.defaults, options);
 		this.isPlaying = false;
@@ -47,24 +41,19 @@
 		this.prepareSequence();
 	};
 
-	SpagSequencer.prototype =
-	{
-		// TODO: Understand constructor in the prototype and lowercase.
+	SpagSequencer.prototype = {
 		constructor: SpagSequencer,
 
-		setOption: function (key, value)
-		{
+		setOption: function (key, value) {
 			this.options[key] = value;
 			this.prepareSequence();
 		},
 
-		toggleLoop: function()
-		{
+		toggleLoop: function() {
 			this.options.loop = this.options.loop !== true;
 		},
 
-		prepareSequence: function()
-		{
+		prepareSequence: function() {
 			var fire;
 			var done;
 			for (var i = 1; i <= this.options.steps; i++) {
@@ -83,8 +72,7 @@
 			}
 		},
 
-		addEvent: function( step, calls )
-		{
+		addEvent: function( step, calls ) {
 			var addedEvent = new SequenceEvent(this.element, step, calls );
 			this.sequenceCommands.push( addedEvent );
 
@@ -94,10 +82,9 @@
 			return addedEvent;
 		},
 
-		removeEvent: function(eventToRemove)
-		{
+		removeEvent: function(eventToRemove) {
 			var index = this.sequenceCommands.indexOf(eventToRemove);
-			if(index == -1){
+			if(index == -1) {
 				return false;
 			}
 
@@ -107,53 +94,43 @@
 			return true;
 		},
 
-		removeAllEvents: function()
-		{
+		removeAllEvents: function() {
 			var self = this;
-			for( var i=0; i < this.sequenceCommands.length; i++)
-			{
+			for( var i=0; i < this.sequenceCommands.length; i++) {
 				self.sequenceCommands[i].removeEvents();
 			}
 			this.sequenceCommands.length = 0;
 		},
 
-		getDelayTime: function()
-		{
+		getDelayTime: function() {
 			var beatPerSecond = 60 / this.options.bpm;
 			return beatPerSecond / 4;
 		},
 
-		play: function()
-		{
-			if(this.isPlaying && !this.isPaused)
-			{
+		play: function() {
+			if(this.isPlaying && !this.isPaused) {
 				this.fire();
 			}
-			else if (this.isPaused && this.isPlaying)
-			{
+			else if (this.isPaused && this.isPlaying) {
 				this.fireFromPause();
 			}
 		},
 
-		start: function()
-		{
-			if(this.isPlaying)
-			{
+		start: function() {
+			if(this.isPlaying) {
 				this.stop();
 			}
 			this.isPlaying = true;
 			this.play();
 		},
 
-		pause: function()
-		{
+		pause: function() {
 			this.isPlaying = false;
 			this.isPaused = true;
 			this.clearTimedEvent();
 		},
 
-		stop: function()
-		{
+		stop: function() {
 			this.isPlaying = false;
 			this.isPaused = false;
 			this.afterFire();
@@ -161,36 +138,31 @@
 			this.clearTimedEvent();
 		},
 
-		fire: function()
-		{
+		fire: function() {
 			var self = this;
 			$(this.element).trigger("fire" + this.currentStep);
 			!this.options.reverse ? this.currentStep++ : this.currentStep--;
 			if( this.currentStep > this.options.steps && !this.options.loop ||
 				this.currentStep === 0 && !this.options.loop )
-			{
+      {
 				this.pause();
 			}
-			if(this.currentStep > this.options.steps)
-			{
+			if(this.currentStep > this.options.steps) {
 				this.currentStep = 1;
 			}
-			else if(this.currentStep === 0)
-			{
+			else if(this.currentStep === 0) {
 				this.currentStep = this.options.steps;
 			}
 			this.timeout = setTimeout(function(){self.afterFire();}, (this.getDelayTime() * 1000) );
 		},
 
-		fireFromPause: function()
-		{
+		fireFromPause: function() {
 			this.isPaused = false;
 			this.isPlaying = true;
 			this.afterFire();
 		},
 
-		afterFire: function()
-		{
+		afterFire: function() {
 			var previousStep;
 			if(!this.options.reverse) {
 				previousStep = this.currentStep - 1 === 0 ? this.options.steps : this.currentStep - 1;
@@ -202,8 +174,7 @@
 			this.play();
 		},
 
-		clearTimedEvent: function()
-		{
+		clearTimedEvent: function() {
 			clearTimeout(this.timeout);
 			this.timeout = null;
 		}
@@ -211,34 +182,29 @@
 
 	var old = $.fn.spagSequencer;
 
-	$.fn.spagSequencer = function( options )
-	{
+	$.fn.spagSequencer = function( options ) {
 		var args = Array.prototype.slice.call(arguments, 1);
 
-		if(options == "addEvent")
-		{
+		if(options == "addEvent") {
 			return this.data("spagSequencer").addEvent.apply(this.data("spagSequencer"), args);
 		}
 
-		return this.each( function()
-			{
-				var $this = $(this);
-				var data = $this.data('spagSequencer');
-				if(!data) {
-					$this.data( 'spagSequencer', (data = new SpagSequencer(this, options)) );
-				}
-				if(typeof options == "string")
-				{
-					data[options].apply(data, args);
-				}
-			});
+		return this.each( function() {
+			var $this = $(this);
+			var data = $this.data('spagSequencer');
+			if(!data) {
+				$this.data( 'spagSequencer', (data = new SpagSequencer(this, options)) );
+			}
+			if(typeof options == "string") {
+				data[options].apply(data, args);
+			}
+	    });
 	};
 
 	// TODO: Understand constructor using the jQuery function and uppercase.
 	$.fn.spagSequencer.Constructor = SpagSequencer;
 
-	$.fn.spagSequencer.defaults =
-	{
+	$.fn.spagSequencer.defaults = {
 		bpm:    140,
 		steps:  16,
 		loop:   true,
@@ -246,8 +212,7 @@
 		fireOnAdd: false
 	};
 
-	$.fn.spagSequencer.noConflict = function()
-	{
+	$.fn.spagSequencer.noConflict = function() {
 		$.fn.spagSequencer = old;
 		return this;
 	};

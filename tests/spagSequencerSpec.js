5c128310fa9370bf685dc3d5d464598aa25fdecc
Gabo <gabo.obregon@gmail.com>
2013-09-05 22:18:00 -0400

    Formatting of the test file.

--- tests/spagSequencerSpec.js
+++ tests/spagSequencerSpec.js
@@ -1,66 +1,55 @@
-describe( "Sequencer", function()
-{
+describe( "Sequencer", function() {
 	var sequencer, instance;
 
-	beforeEach( function()
-	{
+	beforeEach( function() {
 		sequencer = $('<div id="sequencerDiv"></div>').spagSequencer({
 			steps: 32
 		});
 		instance = sequencer.data('spagSequencer');
 
 		return $(sequencer).appendTo($('body')).html("<div id='PluginPage'>");
-
 	});
 
-	afterEach( function()
-	{
+	afterEach( function() {
 		sequencer.spagSequencer("stop");
 	});
 
-	it("should return a default bpm of 140", function()
-	{
+	it("should return a default bpm of 140", function() {
 		expect(instance.options.bpm).toEqual(140);
 	});
 
-	it("should have 32 steps defined", function()
-	{
+	it("should have 32 steps defined", function() {
 		expect(instance.options.steps).toEqual(32);
 	});
 
-	it("should have a new bpm of 100 and still have steps at 32", function()
-	{
+	it("should have a new bpm of 100 and still have steps at 32", function() {
 		sequencer.spagSequencer( "setOption", "bpm", 100 );
 		expect(sequencer.data('spagSequencer').options.bpm).toEqual(100);
 		expect(sequencer.data('spagSequencer').options.steps).toEqual(32);
 	});
 
-	it("should have a time interval of 0.25", function()
-	{
+	it("should have a time interval of 0.25", function() {
 		sequencer.spagSequencer("setOption", "bpm", 60);
 		sequencer.spagSequencer("setOption", "steps", 16);
 		var interval = instance.getDelayTime();
 		expect(interval).toEqual(0.25);
 	});
 
-	it("should have a time interval of 0.125", function()
-	{
+	it("should have a time interval of 0.125", function() {
 		sequencer.spagSequencer("setOption", "bpm", 120);
 		sequencer.spagSequencer("setOption", "steps", 16);
 		var interval = instance.getDelayTime();
 		expect(interval).toEqual(0.125);
 	});
 
-	it("function added to sequenceCommand should be called", function()
-	{
+	it("function added to sequenceCommand should be called", function() {
 		var sequenceCommand = jasmine.createSpy( "sequenceCommand" );
 		sequencer.spagSequencer("addEvent", 1, {fire: sequenceCommand});
 		sequencer.trigger("fire1");
 		expect( sequenceCommand ).toHaveBeenCalled();
 	});
 
-	it("length of command sequence should match amount of commands added", function()
-	{
+	it("length of command sequence should match amount of commands added", function() {
 		var sequenceCommand = jasmine.createSpy( "sequenceCommand" );
 		sequencer.spagSequencer("addEvent", 1, {fire: sequenceCommand});
 		sequencer.spagSequencer("addEvent", 1, {fire: sequenceCommand});
@@ -68,15 +57,13 @@ describe( "Sequencer", function()
 		expect(instance.sequenceCommands.length).toEqual(3);
 	});
 
-	it("play should be called", function()
-	{
+	it("play should be called", function() {
 		spyOn(instance, 'play');
 		sequencer.spagSequencer('play');
 		expect(instance.play).toHaveBeenCalled();
 	});
 
-	it("remove all triggers", function()
-	{
+	it("remove all triggers", function() {
 		var sequenceCommand = jasmine.createSpy("sequenceCommand");
 		sequencer.spagSequencer("addEvent", 1, {fire: sequenceCommand});
 		sequencer.spagSequencer("removeAllEvents");
@@ -84,8 +71,7 @@ describe( "Sequencer", function()
 		expect(sequenceCommand).not.toHaveBeenCalled();
 	});
 
-	it("pauses at current step", function()
-	{
+	it("pauses at current step", function() {
 		expect(instance.currentStep).toEqual(1);
 		instance.currentStep = 6;
 		instance.pause();
@@ -93,16 +79,14 @@ describe( "Sequencer", function()
 		expect(instance.currentStep).toEqual(6);
 	});
 
-	it("stops and resets current step", function()
-	{
+	it("stops and resets current step", function() {
 		instance.currentStep = 6;
 		sequencer.spagSequencer('stop');
 		expect(instance.isPlaying).toBe(false);
 		expect(instance.currentStep).toEqual(1);
 	});
 
-	it("should clear timed out function", function()
-	{
+	it("should clear timed out function", function() {
 		spyOn(instance, 'clearTimedEvent');
 		var sequenceCommand = jasmine.createSpy( "sequenceCommand" );
 		sequencer.spagSequencer("addEvent", 1, {fire: sequenceCommand});
@@ -110,8 +94,7 @@ describe( "Sequencer", function()
 		expect(instance.clearTimedEvent).toHaveBeenCalled();
 	});
 
-	it("stop should have correct order of method calls", function()
-	{
+	it("stop should have correct order of method calls", function() {
 		sequencer.spagSequencer("start");
 		spyOn(instance, 'fire');
 		sequencer.spagSequencer("stop");
@@ -119,8 +102,7 @@ describe( "Sequencer", function()
 
 	});
 
-	it("Start called twice should stop and start again", function()
-	{
+	it("Start called twice should stop and start again", function() {
 		sequencer.spagSequencer("start");
 		spyOn(instance, 'stop');
 		spyOn(instance, 'play');
@@ -129,8 +111,7 @@ describe( "Sequencer", function()
 		expect(instance.play).toHaveBeenCalled();
 	});
 
-	it("Should remove single command", function()
-	{
+	it("Should remove single command", function() {
 		var sequenceCommand = jasmine.createSpy("sequenceCommand");
 		var commandToRemove = jasmine.createSpy("commandToRemove");
 		sequencer.spagSequencer("addEvent", 1, {fire: sequenceCommand});
@@ -142,8 +123,7 @@ describe( "Sequencer", function()
 		expect(instance.sequenceCommands.length).toEqual(1);
 	});
 
-	it("Should remove one command out of two identical ones", function()
-	{
+	it("Should remove one command out of two identical ones", function() {
 		var command1 = jasmine.createSpy("command");
 		var command2 = jasmine.createSpy("command");
 		sequencer.spagSequencer("addEvent", 1, {fire: command1});
@@ -157,28 +137,26 @@ describe( "Sequencer", function()
 		expect(command1.callCount).toEqual(2);
 	});
 
-	it("Should call command at correct time", function()
-	{
+	it("Should call command at correct time", function() {
 		var flag = false;
 		var command = jasmine.createSpy("command");
 		sequencer.spagSequencer("addEvent", 2, {fire: command});
 		sequencer.spagSequencer("start");
 
-		runs(function(){
+		runs(function() {
 			setTimeout(function(){flag = true;}, 150);
 		});
 
 		waitsFor(function(){ return flag }, "The command should have been called", 200);
 
-		runs(function()
-		{
+
+		runs(function() {
 			expect(command.callCount).toEqual(1);
 		});
 
 	});
 
-	it("Should pause at the correct time", function()
-	{
+	it("Should pause at the correct time", function() {
 		var command = jasmine.createSpy("command");
 		var afterCommand = jasmine.createSpy("afterCommand");
 		sequencer.spagSequencer("addEvent", 10, {fire: command, afterFire: afterCommand});
@@ -190,13 +168,11 @@ describe( "Sequencer", function()
 		expect(afterCommand.callCount).toEqual(1);
 	});
 
-	it("loop to be set in the options", function()
-	{
+	it("loop to be set in the options", function() {
 		expect(instance.options.loop).toBeTruthy();
 	});
 
-	it("loop turned off should call stop after the last step", function()
-	{
+	it("loop turned off should call stop after the last step", function() {
 		sequencer.spagSequencer("setOption", "loop", false);
 		var command = jasmine.createSpy("fireCommand");
 		var afterCommand = jasmine.createSpy("afterFireCommand");
@@ -209,31 +185,27 @@ describe( "Sequencer", function()
 		expect(instance.currentStep).toEqual(1);
 	});
 
-	it("looping can be toggled", function()
-	{
+	it("looping can be toggled", function() {
 		sequencer.spagSequencer("toggleLoop");
 		expect(instance.options.loop).toBeFalsy();
 		sequencer.spagSequencer("toggleLoop");
 		expect(instance.options.loop).toBeTruthy();
 	});
 
-	it("reverse will make the current step the previous one", function()
-	{
+	it("reverse will make the current step the previous one", function() {
 		sequencer.spagSequencer("setOption", "reverse", true);
 		instance.fire();
 		expect(instance.currentStep).toEqual(32);
 	});
 
-	it("should fire on add if option is enabled", function()
-	{
+	it("should fire on add if option is enabled", function() {
 		sequencer.spagSequencer("setOption", "fireOnAdd", true);
 		var command = jasmine.createSpy("fireOnAddCommand");
 		sequencer.spagSequencer("addEvent", 1, {fire: command});
 		expect(command).toHaveBeenCalled();
 	});
 
-	it("should not fire on add when option is disabled", function()
-	{
+	it("should not fire on add when option is disabled", function() {
 		var command = jasmine.createSpy("fireOnAddCommand");
 		sequencer.spagSequencer("addEvent", 1, {fire: command});
 		expect(command).not.toHaveBeenCalled();

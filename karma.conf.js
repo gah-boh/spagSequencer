e1b5c8cd887011484234a2db46f3c17487651074
Gabriel Obregon <gabo.obregon@gmail.com>
2014-02-08 23:52:52 -0500

    Updated karma config to work with the recent version of karma.

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,70 @@
 // Karma configuration
-// Generated on Wed Apr 24 2013 21:30:37 GMT-0400 (EDT)
+// Generated on Sat Feb 08 2014 23:48:43 GMT-0500 (EST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['jasmine'],
 
 
-// list of files / patterns to load in the browser
-files = [
-	JASMINE,
-	JASMINE_ADAPTER,
-
-	// Libraries
-	'examples/scripts/jquery/jquery-1.9.1.js',
-
-	// Source
-	'spagSequencer.js',
-
-	// Tests
-	'tests/*Spec.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'examples/scripts/jquery/jquery-1.9.1.js',
+      'spagSequencer.js',
+      'tests/*Spec.js'
+    ],
 
 
-// list of files to exclude
-exclude = [
-
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
 
-// web server port
-port = 9898;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
 
-// cli runner port
-runnerPort = 8989;
+    // web server port
+    port: 9876,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['Chrome'],
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
